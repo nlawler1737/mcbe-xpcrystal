64a3741b92d1ced36a9ed4b242cdf3100460166d
nlawler1737 <72905287+nlawler1737@users.noreply.github.com>
2025-02-05 22:21:31 -0800

    summon xp, organize added ability to summon xp at feet added interact button for mobile

---
+++ BP/scripts/xpcrystal/classes/XpCrystalItemStack.ts
@@ -0,0 +1,107 @@
+import { ContainerSlot } from "@minecraft/server"
+import { getDisplayPercent } from "../utils"
+import { EXPERIENCE_DYNAMIC_ID, STOREAGE_LORE_ID, MAX_XP_STORAGE } from "../constants"
+
+export class XpCrystalItemStack {
+    #slot: ContainerSlot;
+    #xpLore: string;
+
+    constructor(xpCrystalSlot: ContainerSlot) {
+        if (xpCrystalSlot.getItem().typeId !== "xpcrystal:xpcrystal") {
+            throw new Error("'xpCrystal' must be of type 'xpcrystal:xpcrystal'")
+        }
+        this.#slot = xpCrystalSlot
+    }
+
+    get xp() {
+        const val = this.#slot.getDynamicProperty(EXPERIENCE_DYNAMIC_ID) as number;
+        if (val == undefined) {
+            this.#slot.setDynamicProperty(EXPERIENCE_DYNAMIC_ID, 0)
+            return 0
+        }
+        return val
+    }
+
+    get xpLore() {
+        return this.#xpLore
+    }
+
+    get slot() {
+        return this.#slot
+    }
+
+    addExperience(add: number) {
+        return this.setExperience(this.xp + add)
+    }
+
+    setExperience(value: number) {
+        const newValue = Math.min(Math.max(value, 0), MAX_XP_STORAGE)
+        const added = newValue - this.xp
+
+        this.#updateXp(newValue)
+        return {
+            added,
+            xp: newValue
+        }
+    }
+
+    /**
+     * Updates the item's dynamic property of `EXPERIENCE_DYNAMIC_ID`
+     * and the item's lore using `xp`
+     * 
+     * `set` is used to set the xp value or add to the current value
+     * 
+     * `set` is `true` by default
+     * 
+     * @returns updated crystal lore string
+     */
+    #updateXp(xp: number, set = true) {
+        if (set) {
+            this.#slot.setDynamicProperty(EXPERIENCE_DYNAMIC_ID, xp)
+        } else {
+            this.#slot.setDynamicProperty(EXPERIENCE_DYNAMIC_ID, this.xp + xp)
+        }
+        return this.#updateLore()
+    }
+
+    /**
+     * Sets the experience lore of `this.#item`
+     * 
+     * Uses the item's dynamic property of `EXPERIENCE_DYNAMIC_ID`
+     * to set the value
+     * 
+     * @returns new lore string for item
+     */
+    #updateLore() {
+        const currentXpValue = this.xp;
+        this.#xpLore = this.#getXpStorageLoreString(currentXpValue)
+        this.#slot.setLore([this.#xpLore])
+        return this.#xpLore
+    }
+
+    #getXpStorageLoreString(value: number) {
+        const percent = getDisplayPercent((value / MAX_XP_STORAGE) * 100)
+        let color: string;
+        if (percent === 100) {      // 100
+            color = "b"
+        } else if (percent === 0) { // 0
+            color = "n"
+        } else if (percent > 90) {  // 91 - 99
+            color = "a"
+        } else if (percent > 70) {  // 71 - 90
+            color = "e"
+        } else if (percent > 50) {  // 51 - 70
+            color = "g"
+        } else if (percent > 30) {  // 31 - 50
+            color = "6"
+        } else if (percent > 10) {  // 11 - 30
+            color = "v"
+        } else {                    // 1 - 10
+            color = "c"
+        }
+        const str = `${STOREAGE_LORE_ID}§7XP: §${color}${percent}% §7(${value} / ${MAX_XP_STORAGE})`
+        return str
+    }
+}
+
+export default XpCrystalItemStack
---
+++ BP/scripts/xpcrystal/constants.ts
@@ -0,0 +1,8 @@
+export const MAX_XP_STORAGE = 10252 // 64 levels
+export const MAX_XP_SUMMON = 50
+export const MIN_X_HEAD_ROTATION = 80
+export const MAX_XP_SPAWN_DISTANCE = 1.01
+
+export const STOREAGE_LORE_ID = `§${String.fromCharCode(255)}§r`
+export const MODE_LORE_ID = `§${String.fromCharCode(254)}§r`
+export const EXPERIENCE_DYNAMIC_ID = "xpcrystal:experience"
