e3612d9a4449e4e8bd55c2aeae828bb59eefb6fd
nlawler1737 <72905287+nlawler1737@users.noreply.github.com>
2025-02-06 15:12:38 -0800

    change xp orb drop count

--- BP/scripts/xpcrystal/constants.ts
+++ BP/scripts/xpcrystal/constants.ts
@@ -1,5 +1,5 @@
 export const MAX_XP_STORAGE = 10252 // 64 levels
-export const MAX_XP_SUMMON = 50
+export const MAX_XP_SUMMON = 100
 export const MIN_X_HEAD_ROTATION = 80
 export const MAX_XP_SPAWN_DISTANCE = 1.01
 
